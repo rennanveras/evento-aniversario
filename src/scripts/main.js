065c93d075bf2f98ab3a8f91fa2dadd33c58ca65
rennan veras <rennanhenriqueneves@gmail.com>
2023-05-26 14:09:49 -0300

    crianção do hero e das seções do evento, e o timer até o dia do evento foram feitos

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,25 @@
-console.log("ola")
+AOS.init();
+
+const dataEvento = new Date("Dec 07, 2023 13:00:00");
+const timeStampEvento = dataEvento.getTime();
+
+const contaHoras= setInterval(() => {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distaciaAteEvento = timeStampEvento - timeStampAtual;
+
+    const dias = Math.floor(distaciaAteEvento / (1000 * 60 * 60 * 24));
+    const horas = Math.floor((distaciaAteEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutos = Math.floor((distaciaAteEvento % (1000 * 60 * 60)) / (1000 * 60));
+    const segundos = Math.floor((distaciaAteEvento % (1000 * 60)) / 1000);
+
+    const contagemFinal = `${dias}d ${horas}h ${minutos}m ${segundos}s`
+    document.getElementById('contador').innerText = contagemFinal;
+
+    if(distaciaAteEvento < 0){
+        clearInterval(contaHoras);
+
+        document.getElementById('contador').innerText = `Evento expirado`;
+    }
+}, 1000);
